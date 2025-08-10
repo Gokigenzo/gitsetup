98b997751b86642b3c385349bb6aed1227c45166
Gokigenzo <lthpxb2007@gmail.com>
2025-08-10 14:46:26 +0700

    create new file

---
+++ cinout.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() 
+{   
+    int n;
+    std::cout << "Hello, World!" << std::endl;
+    std::cin>>n;
+    
+    std::cout << "You entered: " << n << std::endl;
+    std::cout << "Goodbye!" << std::endl;
+    return 0;
+}
